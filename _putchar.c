3837573f511bdfba663da7fb7815b4857c9f27d6
benjah05 <senzelebenjamine@gmail.com>
2024-07-26 11:06:40 -0700

    Create main.h header file & custom putchar()

---
+++ _putchar.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _putchar - print a character
+ * @c: the character
+ * Return: nothing
+ */
+void _putchar(char c)
+{
+	write (1, &c, 1);
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <stdio.h>
+void _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif
