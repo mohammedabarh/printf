1e25bd03b4d36bffc6b618e37c5d17ae58e0ed96
mohammedabarh <abmohammed606@gmail.com>
2024-07-26 21:00:20 -0700

    FUNCTIONS PRINT

--- main.h
+++ main.h
@@ -1,8 +1,19 @@
 #ifndef _MAIN_H
 #define _MAIN_H
-
+#include <stdarg.h>
 #include <stdio.h>
-void _putchar(char c);
+#include <unistd.h>
+
+
+
+
+int _strlen(char *s)
+int print_s(va_list val)
+int print_c(va_list val);
+int _putchar(char c);
 int _printf(const char *format, ...);
+
+
+
 
 #endif
