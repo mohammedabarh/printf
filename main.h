606660acb59c863ab754f57161f4f80eb45cc830
mohammedabarh <abmohammed606@gmail.com>
2024-07-28 09:17:34 -0700

    Handle NULL cases

--- main.h
+++ main.h
@@ -9,7 +9,7 @@
 /*Function declarations*/
 int print_37(void);
 int _strlen(char *s);
-int print_str(char *s);
+int print_str(va_list val)
 int print_char(va_list val);
 int print_int(va_list val);
 void _putchar(char c);
