1d8d7065c3a65bfa76c60378147198b1a65aaacb
mohammedabarh <abmohammed606@gmail.com>
2024-07-31 17:11:52 -0700

    update

--- print_rev.c
+++ print_rev.c
@@ -2,29 +2,28 @@
 #include <stdarg.h>
 
 /**
- * print_rev - Outputs a string in reverse order.
- * @val: A va_list containing the string argument.
+ * print_rev - Function that prints a string in reverse.
+ * @val: A va_list containing the string to print.
  *
- * Return: The length of the string.
+ * Return: The number of characters printed.
  */
 int print_rev(va_list val)
 {
-	char *string = va_arg(val, char*);
-	int length = 0;
+	char *s = va_arg(val, char *);
 	int i;
+	int length = 0;
 
-	if (string == NULL)
-	{
-		return (-1);
-	}
-	while (*(string + length) != '\0')
-	{
+	if (s == NULL)
+		s = "(null)";
+
+	/* Calculate the length of the string */
+	while (s[length] != '\0')
 		length++;
-	}
+
+	/* Print characters in reverse order */
 	for (i = length - 1; i >= 0; i--)
-	{
-		_putchar(*(string + i));
-	}
+		_putchar(s[i]);
+
 	return (length);
 }
 
