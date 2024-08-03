3be22b10b838e54e1d59eb468e35baa68e252a91
mohammedabarh <abmohammed606@gmail.com>
2024-08-03 14:25:01 -0700

    update

--- find_size.c
+++ find_size.c
@@ -1,13 +1,13 @@
 #include "main.h"
-#include <stdarg.h>
 
 /**
  * find_size - determines the size (length modifier) for formatting
  * @format: The string that includes format specifications
  * @i: Position of the current character in the format string
- * @val: List of arguments (not used in this function)
+ * @val: Argument list to access any further parameters if needed
  *
- * Return: size value (1 for 'l', -1 for 'h', 0 for none)
+ * Return: size value (1 for 'l', -1 for 'h', 0 for none, or
+ *         any other dynamic size)
  */
 int find_size(const char *format, int *i, va_list val)
 {
@@ -23,6 +23,12 @@ int find_size(const char *format, int *i, va_list val)
 		size = -1;
 		(*i)++;
 	}
+	else if (format[*i + 1] == '*')
+	{
+		size = va_arg(val, int);
+		(*i)++;
+	}
 
 	return (size);
 }
+
