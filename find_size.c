c384ab9c97fe6c49f86ebd571363168a8d5e8ff9
mohammedabarh <abmohammed606@gmail.com>
2024-08-03 10:20:53 -0700

    size

---
+++ find_size.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * find_size - determines the size (length modifier) for formatting
+ * @format: The string that includes format specifications
+ * @i: Position of the current character in the format string
+ *
+ * Return: size value (1 for 'l', -1 for 'h', 0 for none)
+ */
+int find_size(const char *format, int *i)
+{
+	int size = 0;
+
+	if (format[*i + 1] == 'l')
+	{
+		size = 1;
+		(*i)++;
+	}
+	else if (format[*i + 1] == 'h')
+	{
+		size = -1;
+		(*i)++;
+	}
+
+	return (size);
+}
+
