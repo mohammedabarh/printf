ceda502e049521b01cc0c7ec52c3a8757be1695a
mohammedabarh <abmohammed606@gmail.com>
2024-08-03 11:22:02 -0700

    size

--- find_size.c
+++ find_size.c
@@ -1,13 +1,15 @@
 #include "main.h"
+#include <stdarg.h>
 
 /**
  * find_size - determines the size (length modifier) for formatting
  * @format: The string that includes format specifications
  * @i: Position of the current character in the format string
+ * @val: Variable argument list (unused in this function)
  *
  * Return: size value (1 for 'l', -1 for 'h', 0 for none)
  */
-int find_size(const char *format, int *i)
+int find_size(const char *format, int *i, va_list val)
 {
 	int size = 0;
 
