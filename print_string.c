c673889a4cabc87f21319c7e284155b8346e19ab
benjah05 <senzelebenjamine@gmail.com>
2024-08-04 15:25:27 -0700

    Handle precision with string

--- print_string.c
+++ print_string.c
@@ -8,16 +8,15 @@
 int print_string(va_list val, flags *f)
 {
 	char *s = va_arg(val, char*);
-	int i = 0, j, len = 0, length;
+	int i = 0, len = 0, length;
 
 	if (s == NULL)
 		s = "(null)";
 	length = strlen(s);
-	if (f->is_prec && f->prec > length)
-	{
-		for (j = 0; j < f->prec - length; j++)
-			len += _putchar(' ');
-	}
+	if (f->is_prec && f->prec == 0)
+		return (0);
+	if (f->is_prec && f->prec < length)
+		s[f->prec] = '\0';
 	while (s[i])
 	{
 		_putchar(s[i]);
